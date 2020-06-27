d004158c29848319f5c6296485edca33848bbbe4
Tanzeel <65041070+tanzeel69@users.noreply.github.com>
2020-06-27 14:42:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,4 +14,14 @@ function totalAns(){
     var abc  = document.getElementById("outputAnswer");
     abc.value=eval(abc.value)
 }
- 
+
+
+function cutAns(){
+    var abc  = document.getElementById("outputAnswer").value;
+    var lenAbc = abc.length;
+    for (var i=0;i<lenAbc;i++){}
+    var removeDigit=i-1
+    var finale = abc.slice(0,removeDigit);
+    console.log(finale);
+    document.getElementById("outputAnswer").value=finale
+}
